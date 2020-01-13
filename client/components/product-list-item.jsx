6d2b1ba38aed55384adf80108718e0942cec83af
Elliot Park <elliot.jun.park@gmail.com>
2020-01-13 22:50:32 +0000

    Added flex class to card-body in ProductListItem

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -11,7 +11,7 @@ const ProductListItem = props => {
   return (
     <div className="card mb-3 mx-3" style={cardStyle}>
       <img src={props.image} className="card-img-top" style={imageStyle} alt="Busted" />
-      <div className="card-body">
+      <div className="card-body d-flex flex-column justify-content-around">
         <h5 className="card-title">{props.name}</h5>
         <h6 className="card-subtitle text-muted">{'$' + (props.price / 1000).toFixed(2)}</h6>
         <p className="card-text">{props.shortDescription}</p>
