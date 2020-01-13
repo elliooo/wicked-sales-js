b5fa89ef82bffa41eb168d4d93d7607265578d02
Elliot Park <elliot.jun.park@gmail.com>
2020-01-13 22:47:29 +0000

    Adding margin class to Header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = props => {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid mb-3">
       <div className="row">
         <div className="col bg-dark text-light pl-5 p-2">
           <h2><span className="fas fa-dollar-sign" /> Wicked Sales</h2>
